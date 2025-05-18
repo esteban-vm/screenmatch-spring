e36c419b408ace59bce68d9dc11fe80f7ffb6935
Esteban V.M. <estebanvm90@outlook.com>
2025-05-18 15:43:11 -0300

    prueba API y Conversor de datos

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,18 +1,25 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.models.Series;
+import com.aluracursos.screenmatch.services.API;
+import com.aluracursos.screenmatch.services.DataConversor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
-
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
-        System.out.println("Hola mundo desde Spring");
+        API api = new API();
+        var json = api.getData("http://www.omdbapi.com/?apikey=9c53b1c8&t=matrix");
+
+        DataConversor conversor = new DataConversor();
+        var data = conversor.getData(json, Series.class);
+        System.out.println(data);
     }
 }
