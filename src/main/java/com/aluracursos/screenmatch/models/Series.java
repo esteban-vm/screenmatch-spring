4c10a530231642d30480ee3e8ded2ea18399eae9
Esteban V.M. <estebanvm90@outlook.com>
2025-05-18 15:42:29 -0300

    agrega anotación para ignorar propiedades

--- src/main/java/com/aluracursos/screenmatch/models/Series.java
+++ src/main/java/com/aluracursos/screenmatch/models/Series.java
@@ -1,7 +1,9 @@
 package com.aluracursos.screenmatch.models;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Series(
         @JsonAlias("Title") String title,
         @JsonAlias("imdbRating") String rating,
