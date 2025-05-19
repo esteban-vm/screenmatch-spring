53ac84c22794a72d77cea24cd7f5d332e5bbd7fb
Esteban V.M. <estebanvm90@outlook.com>
2025-05-19 11:08:44 -0300

    actualiza Series.java

--- src/main/java/com/aluracursos/screenmatch/models/Series.java
+++ src/main/java/com/aluracursos/screenmatch/models/Series.java
@@ -7,6 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record Series(
         @JsonAlias("Title") String title,
         @JsonAlias("imdbRating") String rating,
-        String totalSeasons
+        @JsonAlias("totalSeasons") int numberOfSeasons
 ) {
 }
