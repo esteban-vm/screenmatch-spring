ab0495bae1bece266996ca8aa0acae71a6cc4ae8
Esteban V.M. <estebanvm90@outlook.com>
2025-05-19 11:09:07 -0300

    prueba temporadas

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,15 @@
 package com.aluracursos.screenmatch;
 
 import com.aluracursos.screenmatch.models.Episode;
+import com.aluracursos.screenmatch.models.Season;
 import com.aluracursos.screenmatch.models.Series;
 import com.aluracursos.screenmatch.services.API;
 import com.aluracursos.screenmatch.services.DataConversor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -19,12 +22,22 @@ public class ScreenmatchApplication implements CommandLineRunner {
         API api = new API();
         DataConversor conversor = new DataConversor();
 
-        var json = api.getData("http://www.omdbapi.com/?apikey=9c53b1c8&t=game-of-thrones");
+        var json = api.call("http://www.omdbapi.com/?apikey=9c53b1c8&t=game-of-thrones");
         var series = conversor.getData(json, Series.class);
         System.out.println(series);
 
-        json = api.getData("http://www.omdbapi.com/?apikey=9c53b1c8&t=game-of-thrones&Season=1&episode=1");
+        json = api.call("http://www.omdbapi.com/?apikey=9c53b1c8&t=game-of-thrones&Season=1&episode=1");
         var episode = conversor.getData(json, Episode.class);
         System.out.println(episode);
+
+        var seasons = new ArrayList<Season>();
+
+        for (int i = 1; i <= series.numberOfSeasons(); i++) {
+            json = api.call("http://www.omdbapi.com/?apikey=9c53b1c8&t=game-of-thrones&Season=" + i);
+            var season = conversor.getData(json, Season.class);
+            seasons.add(season);
+        }
+
+        seasons.forEach(System.out::println);
     }
 }
