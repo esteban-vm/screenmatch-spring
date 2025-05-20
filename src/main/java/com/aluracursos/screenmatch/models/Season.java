04c84f43936d7b830fa613b0bbd85666a13549c3
Esteban V.M. <estebanvm90@outlook.com>
2025-05-19 22:26:45 -0300

    actualiza Season.java

--- src/main/java/com/aluracursos/screenmatch/models/Season.java
+++ src/main/java/com/aluracursos/screenmatch/models/Season.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Season(
-        @JsonAlias("Season") String number,
+        @JsonAlias("Season") int number,
         @JsonAlias("Episodes") List<Episode> episodes
 ) {
 }
