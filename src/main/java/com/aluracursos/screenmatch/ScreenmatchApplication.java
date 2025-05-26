5eb6e782de303998d17d33c925747daa8d4a624d
Esteban V.M. <estebanvm90@outlook.com>
2025-05-26 11:33:14 -0300

    implementa repositorio de series

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,19 +1,24 @@
 package com.aluracursos.screenmatch;
 
 import com.aluracursos.screenmatch.main.Main;
+import com.aluracursos.screenmatch.repositories.SeriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
+    @Autowired
+    private SeriesRepository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
-        Main main = new Main();
+        Main main = new Main(repository);
         main.showMenu();
 
         // StreamExample streamExample = new StreamExample();
