e02393f6b6ede03be676a84feb7a39f807b16126
Esteban V.M. <estebanvm90@outlook.com>
2025-05-27 11:44:22 -0300

    actualiza DataSeason.java

--- src/main/java/com/aluracursos/screenmatch/models/DataSeason.java
+++ src/main/java/com/aluracursos/screenmatch/models/DataSeason.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DataSeason(
-        @JsonAlias("DataSeason") int number,
+        @JsonAlias("Season") int number,
         @JsonAlias("Episodes") List<DataEpisode> episodes
 ) {
 }
