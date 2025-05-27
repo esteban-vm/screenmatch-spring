c1b225016629241ee0b7a23b0f5d8a40823deb86
Esteban V.M. <estebanvm90@outlook.com>
2025-05-27 11:44:12 -0300

    actualiza DataEpisode.java

--- src/main/java/com/aluracursos/screenmatch/models/DataEpisode.java
+++ src/main/java/com/aluracursos/screenmatch/models/DataEpisode.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DataEpisode(
         @JsonAlias("Title") String title,
-        @JsonAlias("DataEpisode") int number,
+        @JsonAlias("Episode") int number,
         @JsonAlias("imdbRating") String rating,
         @JsonAlias("Released") String dateOfRelease
 ) {
