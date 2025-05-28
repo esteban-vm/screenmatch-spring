89cbe48a3d73749d664fe5191837fa0edf11304a
Esteban V.M. <estebanvm90@outlook.com>
2025-05-28 17:05:41 -0300

    actualiza SeriesRepository.java

--- src/main/java/com/aluracursos/screenmatch/repositories/SeriesRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repositories/SeriesRepository.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.repositories;
 import com.aluracursos.screenmatch.models.Genre;
 import com.aluracursos.screenmatch.models.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,9 @@ public interface SeriesRepository extends JpaRepository<Series, Long> {
     List<Series> findTop5ByOrderByRatingDesc();
 
     List<Series> findByGenre(Genre genre);
+
+    // List<Series> findBySeasonsLessThanEqualAndRatingGreaterThanEqual(Integer seasons, Double rating);
+
+    @Query("SELECT s FROM Series s WHERE s.seasons <= :seasons AND s.rating >= :rating")
+    List<Series> findBySeasonsAndRating(Integer seasons, Double rating);
 }
