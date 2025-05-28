d3a64ed07c2d4054b50eb596abb10e23061efdef
Esteban V.M. <estebanvm90@outlook.com>
2025-05-28 12:13:27 -0300

    actualiza Genre.java

--- src/main/java/com/aluracursos/screenmatch/models/Genre.java
+++ src/main/java/com/aluracursos/screenmatch/models/Genre.java
@@ -1,21 +1,23 @@
 package com.aluracursos.screenmatch.models;
 
 public enum Genre {
-    ACTION("Action"),
-    ROMANCE("Romance"),
-    COMEDY("Comedy"),
-    DRAMA("Drama"),
-    CRIME("Crime");
+    ACTION("Action", "Acción"),
+    ROMANCE("Romance", "Romance"),
+    COMEDY("Comedy", "Comedia"),
+    DRAMA("Drama", "Drama"),
+    CRIME("Crime", "Crimen");
 
     private final String genreOMDB;
+    private final String genreSpanish;
 
-    Genre(String genreOMDB) {
+    Genre(String genreOMDB, String genreSpanish) {
         this.genreOMDB = genreOMDB;
+        this.genreSpanish = genreSpanish;
     }
 
     public static Genre fromString(String text) {
         for (Genre genre : Genre.values()) {
-            if (genre.genreOMDB.equalsIgnoreCase(text)) {
+            if (genre.genreOMDB.equalsIgnoreCase(text) || genre.genreSpanish.equalsIgnoreCase(text)) {
                 return genre;
             }
         }
