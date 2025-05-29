d3147f736b21d9e7df8f14d665605b7329b9b2c2
Esteban V.M. <estebanvm90@outlook.com>
2025-05-29 11:05:04 -0300

    actualiza SeriesRepository.java

--- src/main/java/com/aluracursos/screenmatch/repositories/SeriesRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repositories/SeriesRepository.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch.repositories;
 
+import com.aluracursos.screenmatch.models.Episode;
 import com.aluracursos.screenmatch.models.Genre;
 import com.aluracursos.screenmatch.models.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,4 +20,11 @@ public interface SeriesRepository extends JpaRepository<Series, Long> {
 
     @Query("SELECT s FROM Series s WHERE s.seasons <= :seasons AND s.rating >= :rating")
     List<Series> findBySeasonsAndRating(Integer seasons, Double rating);
+
+    // @Query("SELECT e FROM Series s JOIN s.episodes e WHERE LOWER(e.title) = LOWER(:episodeTitle)")
+    @Query("SELECT e FROM Series s JOIN s.episodes e WHERE e.title ILIKE %:episodeTitle%")
+    List<Episode> findEpisodesByTitle(String episodeTitle);
+
+    @Query("SELECT e FROM Series s JOIN s.episodes e WHERE s = :series ORDER BY e.rating DESC LIMIT 5")
+    List<Episode> findTop5EpisodesBySeries(Series series);
 }
