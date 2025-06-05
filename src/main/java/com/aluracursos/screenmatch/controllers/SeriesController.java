78ad6fc48ddec5666464ebc4b5ec39366e196edf
Esteban V.M. <estebanvm90@outlook.com>
2025-06-04 22:20:07 -0300

    actualiza SeriesController.java

--- src/main/java/com/aluracursos/screenmatch/controllers/SeriesController.java
+++ src/main/java/com/aluracursos/screenmatch/controllers/SeriesController.java
@@ -1,12 +1,38 @@
 package com.aluracursos.screenmatch.controllers;
 
+import com.aluracursos.screenmatch.dto.SeriesDTO;
+import com.aluracursos.screenmatch.services.SeriesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/series")
 public class SeriesController {
-    @GetMapping("/series")
-    public String showMessage() {
-        return "Hola mundo web";
+    @Autowired
+    private SeriesService service;
+
+    @GetMapping()
+    public List<SeriesDTO> getAllSeries() {
+        return service.getAllSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SeriesDTO> getTop5Series() {
+        return service.getTop5Series();
+    }
+
+    @GetMapping("/lanzamientos")
+    public List<SeriesDTO> getLatestSeries() {
+        return service.getLatestSeries();
+    }
+
+    @GetMapping("/{id}")
+    public SeriesDTO getSeriesById(@PathVariable Long id) {
+        return service.getSeriesById(id);
     }
 }
