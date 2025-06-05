4c7616e4c0b073b4964f6e85fd50474efa6c12d2
Esteban V.M. <estebanvm90@outlook.com>
2025-06-05 10:41:54 -0300

    actualiza SeriesService.java

--- src/main/java/com/aluracursos/screenmatch/services/SeriesService.java
+++ src/main/java/com/aluracursos/screenmatch/services/SeriesService.java
@@ -1,6 +1,9 @@
 package com.aluracursos.screenmatch.services;
 
+import com.aluracursos.screenmatch.dto.EpisodeDTO;
 import com.aluracursos.screenmatch.dto.SeriesDTO;
+import com.aluracursos.screenmatch.models.Episode;
+import com.aluracursos.screenmatch.models.Genre;
 import com.aluracursos.screenmatch.models.Series;
 import com.aluracursos.screenmatch.repositories.SeriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +11,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SeriesService {
     @Autowired
     private SeriesRepository repository;
 
-    private List<SeriesDTO> convertData(List<Series> series) {
+    private List<SeriesDTO> convertSeries(List<Series> series) {
         return series
                 .stream()
                 .map(s -> new SeriesDTO(
@@ -30,16 +34,27 @@ public class SeriesService {
                 .toList();
     }
 
+    private List<EpisodeDTO> convertEpisodes(List<Episode> episodes) {
+        return episodes
+                .stream()
+                .map(episode -> new EpisodeDTO(
+                        episode.getTitle(),
+                        episode.getSeason(),
+                        episode.getNumber())
+                )
+                .collect(Collectors.toList());
+    }
+
     public List<SeriesDTO> getAllSeries() {
-        return convertData(repository.findAll());
+        return convertSeries(repository.findAll());
     }
 
     public List<SeriesDTO> getTop5Series() {
-        return convertData(repository.findTop5ByOrderByRatingDesc());
+        return convertSeries(repository.findTop5ByOrderByRatingDesc());
     }
 
     public List<SeriesDTO> getLatestSeries() {
-        return convertData(repository.findLatestReleases());
+        return convertSeries(repository.findLatestReleases());
     }
 
     public SeriesDTO getSeriesById(Long id) {
@@ -58,5 +73,21 @@ public class SeriesService {
                 series.getSynopsis()
         );
 
+    }
+
+    public List<EpisodeDTO> getAllEpisodes(Long id) {
+        Optional<Series> optionalSeries = repository.findById(id);
+        if (optionalSeries.isEmpty()) return null;
+        Series series = optionalSeries.get();
+        return convertEpisodes(series.getEpisodes());
+    }
+
+    public List<EpisodeDTO> getSeasonByNumber(Long id, Long season) {
+        return convertEpisodes(repository.findSeasonByNumber(id, season));
+    }
+
+    public List<SeriesDTO> getSeriesByGenre(String genre) {
+        Genre category = Genre.fromString(genre);
+        return convertSeries(repository.findByGenre(category));
     }
 }
